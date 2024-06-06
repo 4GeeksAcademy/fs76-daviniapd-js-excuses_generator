54e649b65db20e71e0428c562a1b38d159316d66
Davinia pd <delgadavi@gmail.com>
2024-06-06 11:38:49 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./index.html";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "The turtle",
+    "My dad",
+    "The policeman",
+    "My dog",
+    "My cousin's horse"
+  ];
+  let action = ["hit", "kicked", "smashed", "broke", "stole"];
+  let what = ["my computer", "my telephone", "my bike", "the keys"];
+  let when = [
+    "while I was doing yoga",
+    "when I was taking a shower",
+    "when I was in bed",
+    "before the work",
+    "while I was food shopping"
+  ];
+  let rand_who = Math.floor(Math.random() * who.length);
+  let rand_action = Math.floor(Math.random() * action.length);
+  let rand_what = Math.floor(Math.random() * what.length);
+  let rand_when = Math.floor(Math.random() * when.length);
+  //return who[i] + action[i] + what[i] + when[i];
+  document.getElementById("excuse").innerHTML =
+    "<p>" +
+    who[rand_who] +
+    " " +
+    action[rand_action] +
+    " " +
+    what[rand_what] +
+    " " +
+    when[rand_when] +
+    "</p>";
 };
+//for (let i = who; i < who.length; i++) {}
+console.log("OMG! You will not believe me but...");
